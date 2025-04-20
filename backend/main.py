958c2d8e6414384fdc374be8c091acef85755f31
Daniel <nguyendaniel1312@gmail.com>
2025-04-19 18:32:46 -0700

    merge frontend with new backend functions

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,8 @@ import mediapipe as mp
 import os
 from dotenv import load_dotenv
 import tempfile
+from db import test_connection
+from routers import auth0_users
 
 
 # ========== CONFIG ==========
@@ -15,8 +17,6 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-from db import test_connection
-from routers import auth0_users
 app = FastAPI(title="HackDavis API")
 
 # ========== CORS CONFIG ==========
